3ae0639f4d7d0f76026628e6f7f1a4a3d694622d
unknown <71107476+dpw462@users.noreply.github.com>
2021-02-25 14:35:37 -0800

    adding portfolio program

--- python_code/portfolios/portfolio.py
+++ python_code/portfolios/portfolio.py
@@ -1,130 +1,116 @@
+'''
+author: d. wells
+year:2021
+purpose: 
+- create & analyze portfolios with CAPM
+- program needs metrics.py
+'''
 import numpy as np
 import pandas as pd
 from pandas_datareader import data as pdr
 import matplotlib.pyplot as plt
 from datetime import date
+from metrics import GetMetrics as Metrics
 
 def get_portfolio_data(today):
-    pfolio_1 = []
-    pfolio_2 = []
+    pfolio = [] # create portfolio
+    confirm = 'n'
     try:
-        ## basket 1
-        investment_1 = input('\n[+] Add investment(s) to first portfolio (separate by ";"): ')
-        if ';' in investment_1:
-            for stock in investment_1.split(';'):
-                stock = stock.strip(' ')
-                pfolio_1.append(stock)
-        else:
-            pfolio_1.append(investment_1)
-        start_term_1 = input('[+] Enter START for historical prices (yyyy-mm-dd): ')
-        end_term_1 = input('[+] Enter END for terminal date (yyyy-mm-dd), or default, "d", for today: ')
-        if end_term_1.lower() == 'd':
-            end_term_1 = today
-        print(pfolio_1)
-        basket1 = float(len(pfolio_1))
-        #print(basket_1)
-        portfolio_1_data = get_yahoo_data(pfolio_1, start_term_1, end_term_1)
+        while confirm.lower().strip() == 'n':
+            ## basket of tickers
+            investment = input('\n[+] Add investment(s) to portfolio (separate by ";"): ')
+            if ';' in investment:
+                for stock in investment.split(';'):
+                    stock = stock.strip(' ')
+                    pfolio.append(stock)
+            else:
+                pfolio.append(investment)
+            confirm = input(f'\n[+] Process portfolio {pfolio} <"N", else any key to proceed>?: ')
         
-        ## basket 2
-        investment_2 = input('\n[+] Add investment(s) to second portfolio (separate by ";"): ')
-        if ';' in investment_2:
-            for stock in investment_2.split(';'):
-                stock = stock.strip(' ')
-                pfolio_2.append(stock)
-        else:
-            pfolio_2.append(investment_2)
-        start_term_2 = input('[+] Enter START for historical prices (yyyy-mm-dd): ')
-        end_term_2 = input('[+] Enter END for terminal date (yyyy-mm-dd), or default, "d", for today: ')
-        if end_term_2.lower() == 'd':
-            end_term_2 = today
-        print(pfolio_2)
-        basket2 = float(len(pfolio_2))
-        #print(basket_2)
-        portfolio_2_data = get_yahoo_data(pfolio_2, start_term_2, end_term_2)
+        start_term = input('\n[+] Enter START for historical prices (yyyy-mm-dd): ')
+        end_term = input('[+] Enter END for terminal date (yyyy-mm-dd), or default, "d", for today: ')
+        if end_term.lower().strip() == 'd':
+            end_term = today
+        basket = float(len(pfolio))
+        portfolio_data = get_yahoo_data(pfolio, start_term, end_term)
+                
+        ## call plotting of portfolio
+        plot_port = input('\n[+] Plot graph (output to PDF) <"Y", else any key>?: ')
+        if (plot_port.lower().strip() == 'y'):
+            plot_portfolio(portfolio_data, 'Portfolio')
         
-        ## call plotting of portfolios
-        #plot_portfolios(portfolio_1_data, portfolio_2_data)
-        ## get returns on portfolios
-        get_portfolio_return(basket1, portfolio_1_data, basket2, portfolio_2_data)
+        ## get return on portfolio
+        get_portfolio_return(basket, portfolio_data, start_term, end_term)    
     except:
         print('\n[*] Error: input errors!')
         exit
     
 def get_yahoo_data(pfolio, start_term, end_term):
+    ''' fetch from Yahoo Finance API '''
     portfolio = pd.DataFrame()
     for s in pfolio:
         portfolio[s] = pdr.DataReader(s, data_source='yahoo', start=start_term, end=end_term)['Adj Close']
-    
     return portfolio
     
-def plot_portfolios(portfolio1, portfolio2):
-    ## section for PLOTTING prices
-    port1 = portfolio1.iloc[0]
-    (portfolio1 / port1 * 100).plot(figsize=(15,10))
-    port2 = portfolio2.iloc[0]
-    (portfolio2 / port2 * 100).plot(figsize=(15,10))
+def plot_portfolio(portfolio, filename):
+    ''' section for PLOTTING performance '''
+    port = portfolio.iloc[0]
+    (portfolio / port * 100).plot(figsize=(15,10))
+    plt.xlabel('Time', fontsize=18)
+    plt.ylabel('Security Returns (Normalized)', fontsize=18)
+    plt.title('Portfolio Performance', fontsize=24)
+    plot_file = f'{filename}.pdf'
+    plt.savefig(plot_file)
     plt.show()
-
-def get_portfolio_return(b1, portfolio1, b2, portfolio2):
-    ## (3) section for CALCULATING and DISPLAYING returns
-    # returns all simple returns i.e. rtrn = (current/previous) - 1
+    print(f'\n[*] Graph saved to {plot_file}')
+    
+def get_portfolio_return(b, portfolio, start, end):
+    ''' section for CALCULATING and DISPLAYING returns '''
+    # returns historical simple returns i.e. rtrn = (current/previous) - 1
     # weights (i.e. allocation) need to sum to 1 (i.e. [0.5,0.5] for two funds/tickers)
-    # portfolio 1 weights
-    weights1 = []
-    returns1 = (portfolio1 / portfolio1.shift(1)) - 1
-    weighting_1 = input('\n[+] Portfolio 1: Allocation (in decimal percentage, or "e" for equal distribution)(separate by ";")?: ')
-    if (weighting_1.lower() == 'e'):
-        w1 = 1 / b1
-        for w in range(int(b1)):
-            w = w1
-            weights1.append(w)
-    elif ';' in weighting_1:
-        for w in weighting_1.split(';'):
+    ## portfolio weights
+    weights = []
+    returns = (portfolio / portfolio.shift(1)) - 1
+    weighting = input('\n[+] Portfolio: Allocation (in decimal percentage, "e" for equal)(separate by ";")?: ')
+    if (weighting.lower().strip() == 'e'):
+        wght = 1 / b
+        for w in range(int(b)):
+            w = wght
+            weights.append(w)
+    elif ';' in weighting:
+        for w in weighting.split(';'):
             w = w.strip(' ')
             w = float(w)
-            weights1.append(w)    
+            weights.append(w)    
     else:
-        weights1.append(float(weighting_1))
-    weights_1 = np.array(weights1)
-    #portfolio 2 weights    
-    weights2 = []
-    returns2 = (portfolio2 / portfolio2.shift(1)) - 1
-    weighting_2 = input('[+] Portfolio 2: Allocation (in decimal percentage, or "e" for equal distribution)(separate by ";")?: ')
-    if (weighting_2.lower() == 'e'):
-        w2 = 1 / b2
-        for w in range(int(b2)):
-            w = w2
-            weights2.append(w)
-    elif ';' in weighting_2:
-        for w in weighting_2.split(';'):
-            w = w.strip(' ')
-            w = float(w)
-            weights2.append(w)    
-    else:
-        weights2.append(float(weighting_2))
-    weights_2 = np.array(weights2)
-    # calculate returns        
-    annual_returns1 = returns1.mean() * 250
-    annual_returns2 = returns2.mean() * 250
-    tot_return1 = np.dot(annual_returns1, weights_1)
-    tot_return2 = np.dot(annual_returns2, weights_2)
-    # print results of respective portfolio returns
-    pfolio1 = str(tot_return1.round(4) * 100) + ' %'
-    pfolio2 = str(tot_return2.round(4) * 100) + ' %'
-    print(f'\n[*] Return of Portfolio 1: {pfolio1}')
-    print(f'[*] Return of Portfolio 2: {pfolio2}')
-
-''' main portfolio program here '''
+        weights.append(float(weighting))
+    weights = np.array(weights)
+        
+    ## calculate returns        
+    annual_returns = (returns.mean() * 252) # daily returns ~ 252 trading days
+    tot_return = np.dot(annual_returns, weights)
+       
+    ## print results of respective portfolio returns
+    pfolio = (tot_return * 100).round(3)
+    print(f'\n[*] Portfolio - Historical return: {pfolio}%')
+    
+    ## use CAPM model analysis for portfolios 
+    want_capm = input('\n[+] CAPM Analysis (output to CSV) <"Y", else any key>?: ')
+    if (want_capm.lower().strip() == 'y'):
+        filename = input('\n[+] Name file to write Portfolio: ')
+        met = Metrics(portfolio, start, end, filename)
+        met.get_metrics()
+        
+#############################################################################################################
+''' main program here '''
 if __name__ == '__main__':
     today = str(date.today())
-    print('\n*** PORTFOLIO ANALYSIS ***')
+    print('\n********** PORTFOLIO ANALYSIS **********')
     redo = 'y'
-    try:
-        while redo.lower() == 'y':
-            ## get portfolio data, plot and show returns
-            get_portfolio_data(today)
-            redo = input('\n[+] Repeat <Y, else any key>?: ')
-            redo.strip(' ')
-    except:
-        print('[*] Error: input error!')
-        exit
+    while redo.lower().strip() == 'y':
+        ## get portfolio data, plot and show results
+        get_portfolio_data(today)
+        redo = input('\n[+] Repeat <"Y", else any key>?: ')            
+    print('\n[*] Exiting... bye!')
+    exit
+#############################################################################################################
